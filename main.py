cf4c2f9dd7c70176a49ca796f8f7f688afed5cf6
Eliel <rentcubacar40@gmail.com>
2025-12-26 23:43:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ PRE_CONFIGURATED_USERS = {
         "proxy": "",
         "tokenize": 0
     },
-    "yordante,OrCesarOr,alejandrorosell,imrushh": {
+    "yordante,OrCesarOr,alejandrorosell,imrushh,maykolguille": {
         "cloudtype": "moodle",
         "moodle_host": "https://cursos.uo.edu.cu/",
         "moodle_repo_id": 4,
@@ -2232,6 +2232,7 @@ if __name__ == '__main__':
         main()
     except:
         main()
+
 
 
 
