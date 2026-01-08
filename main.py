5c844f42db81f3ddeac3a22a3e811692a6476be8
Eliel <rentcubacar40@gmail.com>
2026-01-08 18:08:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ PRE_CONFIGURATED_USERS = {
         "cloudtype": "moodle",
         "moodle_host": "https://cursos.uo.edu.cu/",
         "moodle_repo_id": 4,
-        "moodle_user": "eric.serrano",
+        "moodle_user": "desiderio.vazquez",
         "moodle_password": "ElielThali1521..",
         "zips": 99,
         "uploadtype": "evidence",
@@ -2232,6 +2232,7 @@ if __name__ == '__main__':
         main()
     except:
         main()
+
 
 
 
